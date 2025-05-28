1e40f610c2ef745a21da56cee583c46250c27dfd
mario <Mamm1201@users.noreply.github.com>
2025-05-28 10:03:41 -0500

    Formatear código con black para cumplir con CI

--- e-commerce/settings.py
+++ e-commerce/settings.py
@@ -88,9 +88,7 @@ DATABASES = {
         "PASSWORD": os.getenv("DB_PASSWORD"),
         "HOST": os.getenv("DB_HOST"),
         "PORT": os.getenv("DB_PORT"),
-      
     },
-
 }
 
 # Password validation
