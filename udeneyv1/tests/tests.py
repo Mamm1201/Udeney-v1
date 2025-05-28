44532a31cd696f1cdc7fde54282fc8fb5fc0862b
mario <Mamm1201@users.noreply.github.com>
2025-05-28 07:36:18 -0500

    Crear carpeta  udeneyv1/tests/

--- udeneyv1/tests/tests.py
+++ udeneyv1/tests/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
-from .models import Transacciones, DetalleTransaccion
+from ..models import Transacciones, DetalleTransaccion
 from django.utils.timezone import now, timedelta
 
 
