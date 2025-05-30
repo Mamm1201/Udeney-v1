5ab2e014292f561fcd5bd82c34cea4d749896d62
mario <Mamm1201@users.noreply.github.com>
2025-05-29 20:42:22 -0500

    mejar configuración en el archivotailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,18 @@
 /* eslint-env node */
+// module.exports = {
+//   content: [
+//     './index.html',
+//     './src/**/*.{js,jsx,ts,tsx}', // USA slashes / en vez de backslashes \
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './index.html',
-    './src/**/*.{js,jsx,ts,tsx}', // USA slashes / en vez de backslashes \
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
