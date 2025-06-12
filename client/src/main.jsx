8440e81a0d510ce17bf5d6120498facc3c35f81b
mario <Mamm1201@users.noreply.github.com>
2025-06-12 17:45:42 -0500

    preba de CI en main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import AppRoutes from './appRoutes';
 import { CarritoProvider } from './context/CarritoContext';
 
-//import "./postcss.config";
+//Envolver Carrito para qu quede global
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
