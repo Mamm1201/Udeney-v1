94da0b5e1ce5879a26f2bcd5f528a27eebcd69c0
mario <Mamm1201@users.noreply.github.com>
2025-06-29 01:52:53 -0500

    Corrección de importación de os en settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -89,11 +89,11 @@ WSGI_APPLICATION = "ecommerce.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": os.getenv("DB_NAME", "db_ecommerce"),
-        "USER": os.getenv("DB_USER", "root"),
-        "PASSWORD": os.getenv("DB_PASSWORD", "Mario1201*"),
-        "HOST": os.getenv("DB_HOST", "db"),
-        "PORT": os.getenv("DB_PORT", "3306"),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
         "OPTIONS": {
             "charset": "utf8mb4",  # Soporte para emojis y caracteres especiales
             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",  # Mayor validación
